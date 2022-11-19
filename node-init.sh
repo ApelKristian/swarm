32e577874b50627da2eff8159dd6a23e91ad561e
Kristian Apel <73107850+ApelKristian@users.noreply.github.com>
2022-11-19 02:09:07 +0100

    Update node-init.sh

--- node-init.sh
+++ node-init.sh
@@ -20,7 +20,8 @@ sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_
 sudo apt install --no-install-recommends r-base -y
 
 #install R packages
-sudo R -e 'install.packages("tidyverse")'
+sudo R -e 'install.packages("lubridate")'
+sudo R -e 'install.packages("magrittr")'
 sudo R -e 'install.packages("RSelenium")'
 
 
